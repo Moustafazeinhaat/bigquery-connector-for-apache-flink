0ecfa9bcb4f2b8ce585aa6e966c05295368f3b0d
Moustafazeinhaat <147334489+Moustafazeinhaat@users.noreply.github.com>
2025-05-07 13:29:21 +0300

    Update BigQuerySink.java

--- src/main/java/io/aiven/flink/connectors/bigquery/sink/BigQuerySink.java
+++ src/main/java/io/aiven/flink/connectors/bigquery/sink/BigQuerySink.java
@@ -7,6 +7,7 @@ import com.google.cloud.bigquery.StandardSQLTypeName;
 import org.apache.flink.annotation.PublicEvolving;
 import org.apache.flink.api.connector.sink2.Sink;
 import org.apache.flink.api.connector.sink2.SinkWriter;
+import org.apache.flink.api.connector.sink2.WriterInitContext;
 import org.apache.flink.table.data.RowData;
 import org.apache.flink.table.types.logical.LogicalType;
 import org.apache.flink.util.Preconditions;
@@ -29,7 +30,7 @@ public class BigQuerySink implements Sink<RowData> {
   }
 
   @Override
-  public SinkWriter<RowData> createWriter(InitContext context) throws IOException {
+  public SinkWriter<RowData> createWriter(WriterInitContext context) throws IOException {
     return options.getDeliveryGuarantee() == DeliveryGuarantee.EXACTLY_ONCE
         ? new BigQueryStreamingExactlyOnceSinkWriter(fieldNames, fieldTypes, standardSQLTypes, options)
         : new BigQueryStreamingAtLeastOnceSinkWriter(fieldNames, fieldTypes, standardSQLTypes, options);
